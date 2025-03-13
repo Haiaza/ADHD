b93a77cf2ce4cedcab43806d21b494efdd4ea009
haiaza <ajames1342@gmail.com>
2025-03-13 17:45:51 -0400

    service worker - icon TBD

--- background.js
+++ background.js
@@ -1,1 +1,15 @@
 // keep the lights on when the browser/ext isnt in use etc... 
+
+chrome.alarms.onAlarm.addListener((alarm) => {
+    chrome.storage.local.get(['alarms'], ({ alarms = [] }) => {
+        const matchedAlarm = alarms.find(a => `alarm_${a.time}` === alarm.name);
+        if (matchedAlarm) {
+            chrome.notifications.create({
+                type: 'basic',
+                iconUrl: 'icon.png',
+                title: 'ADHD Alarm',
+                message: matchedAlarm.message
+            });
+        }
+    });
+});

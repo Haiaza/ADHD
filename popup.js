7bfd0f54b8c581fd81da8b8e1b071678c2602bba
haiaza <ajames1342@gmail.com>
2025-03-13 15:40:51 -0400

    debounce

--- popup.js
+++ popup.js
@@ -19,5 +19,28 @@ document.addEventListener('DOMContentLoaded', () => {
         const alarmTimestamp = new Date(alarmTime.value).getTime(); //grab epoch time // value for comparison
         const now = Date.now(); //epoch time  // value for comparison 
 
-    })
-})
+        if (alarmTimestamp > now) {
+            chrome.alarms.create(`alarm_${alarmTimestamp}`, {
+                when: alarmTimestamp,
+                periodInMinutes: 0 // one time alarm
+            });
+
+            //save alarm details
+            chrome.storage.local.get(['alarms'], ({ alarms = [] }) => {
+                alarms.push({
+                    time: alarmTimestamp,
+                    message: alarmMsg.value
+                });
+                chrome.storage.local.set({ alarms });
+            });
+        }
+    });
+});
+
+function debounce(func, timeout = 300) {
+    let timer;
+    return (...args) => {
+        clearTimeout(timer);
+        timer = setTimeout(() => { func.apply(this, args); }, timeout);
+    };
+} // logic to throttle user input
